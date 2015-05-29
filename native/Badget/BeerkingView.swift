bb126c007ce9c9f39c8e98f9b02272ba966e4d4e
Jasper Van Damme <jasper.van.damme@student.howest.be>
2015-05-29 21:33:34 +0200

    added pitch/yaw/roll

--- native/Badget/BeerkingView.swift
+++ native/Badget/BeerkingView.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class BeerkingView: UIView {
     
-    let device: UIDevice
+    let angleText: UILabel
     
     override init(frame: CGRect) {
-        self.device = UIDevice.currentDevice()
+        self.angleText = UILabel(frame: CGRectMake(10, frame.height / 2, 300, 40))
         super.init(frame: frame)
         
         self.backgroundColor = UIColor.grayColor()
-        self.device.proximityMonitoringEnabled = true
-        if(self.device.proximityMonitoringEnabled) {
-            NSNotificationCenter.defaultCenter().addObserver(self, selector: "proximityChanged:", name: "UIDeviceProximityStateDidChangeNotification", object: nil)
-        }
-    }
-    
-    func proximityChanged(notification: NSNotification) {
-        println(self.device.proximityState)
+        
+        self.angleText.text = "0.0°"
+        self.angleText.textAlignment = .Center
+        self.addSubview(self.angleText)
     }
     
     required init(coder aDecoder: NSCoder) {
--- native/Badget/ChallengesViewController.swift
+++ native/Badget/ChallengesViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import CoreMotion
 
-class ChallengesViewController: UIViewController {
+class ChallengesViewController: UIViewController, UIScrollViewDelegate {
+    
+    let motionManager = CMMotionManager()
+    let device = UIDevice.currentDevice()
+    var motionLastRoll:Double! = 0
     
     var sliderView:SliderView! {
         get {
@@ -19,7 +24,62 @@ class ChallengesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.sliderView.scrollView.delegate = self
+        self.device.proximityMonitoringEnabled = true
+        if(self.device.proximityMonitoringEnabled) {
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: "proximityChanged:", name: "UIDeviceProximityStateDidChangeNotification", object: nil)
+        }
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        // if beerkingView is in view
+        if(CGRectIntersectsRect(scrollView.bounds, self.sliderView.beerkingView.frame)) {
+            beerkingVisible()
+        }
+    }
+    
+    func proximityChanged(notification: NSNotification) {
+        println("Proximity changed", self.device.proximityState)
+    }
+    
+    func beerkingVisible() {
+        if (self.motionManager.gyroAvailable) {
+            self.motionManager.deviceMotionUpdateInterval = 0.2;
+            self.motionManager.startDeviceMotionUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: { (data: CMDeviceMotion!, error: NSError!) -> Void in
+        
+                var quat:CMQuaternion = data.attitude.quaternion
+                var pitch = atan2(2*(quat.y*quat.z + quat.w*quat.x), quat.w*quat.w - quat.x*quat.x - quat.y*quat.y + quat.z*quat.z)
+                var roll = atan2(2*(quat.y*quat.w - quat.x*quat.z), 1 - 2*quat.y*quat.y - 2*quat.z*quat.z)
+                var yaw = asin(2*(quat.x*quat.z - quat.w*quat.y))
+                myRoll = radiansToDegrees(atan2(2*(quat.y*quat.w - quat.x*quat.z), 1 - 2*quat.y*quat.y - 2*quat.z*quat.z)) ;
+                myPitch = radiansToDegrees(atan2(2*(quat.x*quat.w + quat.y*quat.z), 1 - 2*quat.x*quat.x - 2*quat.z*quat.z));
+                myYaw = radiansToDegrees(2*(quat.x*quat.y + quat.w*quat.z));
+                
+                /* Smoothing, probably not needed
+                if (self.motionLastRoll == 0) {
+                    self.motionLastRoll = Double(roll)
+                }
+                
+                // kalman filtering
+                var q = 0.1; // process noise
+                var r = 0.1; // sensor noise
+                var p = 0.1; // estimated error
+                var k = 0.5; // kalman filter gain
+                
+                var x = Double(self.motionLastRoll)
+                p = p + q
+                k = p / (p + r)
+                x += Double(k * (roll - x))
+                p = (1 - k) * p
+                self.motionLastRoll = Double(x)*/
+                
+                let angle = pitch * roll * yaw * 180/M_PI
+                
+                self.sliderView.beerkingView.angleText.text = String(stringInterpolationSegment: round(angle)) + "°"
+            })
+        }
     }
     
     override func loadView() {
