ce8a2826e58716e1e18b8f9266392eae2d4fb104
Jasper Van Damme <jasper.van.damme@student.howest.be>
2015-06-04 23:57:42 +0200

    get day from api

--- native/Badget/Settings.swift
+++ native/Badget/Settings.swift
@@ -7,10 +7,24 @@
 //
 
 import UIKit
+import Alamofire
 
 struct Settings {
     static let apiUrl = "http://student.howest.be/jasper.van.damme/20142015/MA4/BADGET/api"
     static let startDate = NSDate(timeIntervalSince1970: 1440057600) // 1440057600: 20/08/15 08:00
     static let secondsEndDay = 64800 // 64800: 18h
     static let endDate = NSDate(timeIntervalSince1970: 1440266400) // 1440266400: 22/08/15 18:00
+    static var currentDate = NSDate()
+    
+    static func getDate() -> NSDate {
+        Alamofire.request(.GET, self.apiUrl + "/day").responseJSON { (_, _, data, _) in
+            let json = JSON(data!)
+            let formatter = NSDateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            if let day = json["current_day"].string {
+                self.currentDate = formatter.dateFromString(day)!
+            }
+        }
+        return self.currentDate
+    }
 }
