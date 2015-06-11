5d722b9b9fe9cdfcdcfe0f80c2a15ba74908bb1c
Jasper Van Damme <jasper.van.damme@student.howest.be>
2015-06-12 01:53:57 +0200

    fixed 'jump' when switching to badgesvc

--- native/Badget/BadgesViewController.swift
+++ native/Badget/BadgesViewController.swift
@@ -14,6 +14,7 @@ class BadgesViewController: UIViewController {
         super.viewDidLoad()
         
         self.title = "Badges"
+        self.view.backgroundColor = Settings.bgColor
     }
     
     override func loadView() {
