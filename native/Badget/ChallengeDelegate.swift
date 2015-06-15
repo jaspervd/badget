0a7f7918e686f2e81d3d66ca4130421394adde6b
Jasper Van Damme <jasper.van.damme@student.howest.be>
2015-06-16 01:45:51 +0200

    fixed challengedelegate

---
+++ native/Badget/ChallengeDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ChallengeProtocol.swift
+//  Badget
+//
+//  Created by Jasper Van Damme on 01/06/15.
+//  Copyright (c) 2015 Jasper Van Damme. All rights reserved.
+//
+
+import UIKit
+
+protocol ChallengeDelegate:class {
+    func willShowResult(challenge: ChallengeViewController)
+}
